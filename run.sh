5d875c3ca48f1570c8f989f6c7214d670b735e09
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-07-30 12:05:33 +0100

    Cleaning up

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python train_lda.py --output-topics output/default_top25p25_topics.csv \
-    --output-tags output/default_top25p25_tags.csv \
-    --vis-filename output/default_top25p25_vis.html \
+python train_lda.py --output-topics output/topics.csv \
+    --output-tags output/tags.csv \
+    --vis-filename output/vis.html \
     --numtopics 3 \
     --passes 1 \
     import input/environment_urltext_100.csv
