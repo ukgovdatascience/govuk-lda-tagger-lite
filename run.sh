32181096c04ce9bcd6430e8cf0d3ad99e7b1c8a5
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-07-30 12:00:27 +0100

    Add input and output folders

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python train_lda.py --output-topics default_top25p25_topics.csv \
-    --output-tags default_top25p25_tags.csv \
-    --vis-filename default_top25p25_vis.html \
+python train_lda.py --output-topics output/default_top25p25_topics.csv \
+    --output-tags output/default_top25p25_tags.csv \
+    --vis-filename output/default_top25p25_vis.html \
     --numtopics 3 \
     --passes 1 \
-    import environment_urltext_100.csv
+    import input/environment_urltext_100.csv
